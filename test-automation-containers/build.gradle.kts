05e0063198e15bd9cf89161b59371475fe5c9cc6
Ricardas Buciunas <ricardas.buciunas2@t-mobile.com>
2021-05-11 15:59:51 +0300

    fix: fixed test-automation-containers local deployment

--- test-automation-containers/build.gradle.kts
+++ test-automation-containers/build.gradle.kts
@@ -1,3 +1,6 @@
+plugins {
+    id("com.nortal.test.java-conventions")
+}
 
 dependencies {
     api(libs.testcontainers)
@@ -5,3 +8,5 @@ dependencies {
     api("org.apache.commons:commons-lang3:3.9")
     api("commons-io:commons-io:2.5")
 }
+
+description = "test-automation-containers"
