a39e46d0f6cd77b7d7bd12de2596c84dfbfddcb7
Artūras Jankus <arturas.jankus@nortal.com>
2021-05-11 14:29:11 +0300

    chore: enable publishing to maven local

--- test-automation-containers/build.gradle.kts
+++ test-automation-containers/build.gradle.kts
@@ -1,7 +1,15 @@
+plugins {
+    id("com.nortal.test.java-conventions")
+}
 
 dependencies {
     api(libs.testcontainers)
     api(libs.bundles.springboot)
     api("org.apache.commons:commons-lang3:3.9")
     api("commons-io:commons-io:2.5")
+}
+
+description = "test-automation-containers"
+repositories {
+    mavenCentral()
 }
