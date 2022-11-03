6d35a23eea1257317786a8e0752980cdfb5f2399
Ričardas Bučiūnas <ricardas.buciunas@nortal.com>
2022-11-03 13:10:30 +0200

    Adding new supported spring profile

--- test-automation-core/src/main/kotlin/com/nortal/test/core/configuration/TestAutomationConstants.kt
+++ test-automation-core/src/main/kotlin/com/nortal/test/core/configuration/TestAutomationConstants.kt
@@ -26,8 +26,10 @@ object TestAutomationConstants {
     const val SPRING_PROFILE_CORE = "core"
     const val SPRING_PROFILE_BASE = "base"
     const val SPRING_PROFILE_OVERRIDE = "override"
+    const val SPRING_PROFILE_CI = "ci"
 
-    val ALL_SPRING_PROFILES = listOf(SPRING_PROFILE_CORE, SPRING_PROFILE_BASE, SPRING_PROFILE_OVERRIDE)
+    val ALL_SPRING_PROFILES =
+        listOf(SPRING_PROFILE_CORE, SPRING_PROFILE_BASE, SPRING_PROFILE_OVERRIDE, SPRING_PROFILE_CI, SPRING_PROFILE_CI)
 
     const val FRAMEWORK_BOOTSTRAP_GLUE = "com.nortal.test.core.cucumber.glue"
     const val PROPERTY_BOOTSTRAP_GLUE_APPEND = "test-automation.cucumber.glue-append"
@@ -35,6 +37,7 @@ object TestAutomationConstants {
 
     const val PARALLEL_CONFIG_STRATEGY_PROPERTY = "fixed"
     const val PROPERTY_PARALLEL_EXECUTOR_COUNT = "test-automation.cucumber.execution.parallel.executor-count"
-    const val PROPERTY_PARALLEL_EXECUTION_GROUP_TAGS = "test-automation.cucumber.execution.parallel.execution-group-tags"
+    const val PROPERTY_PARALLEL_EXECUTION_GROUP_TAGS =
+        "test-automation.cucumber.execution.parallel.execution-group-tags"
     const val PROPERTY_PARALLEL_ISOLATION_TAG = "test-automation.cucumber.execution.parallel.isolation-tag"
 }
