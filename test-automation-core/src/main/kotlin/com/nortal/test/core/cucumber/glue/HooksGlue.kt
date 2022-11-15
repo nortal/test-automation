1c5134f05cacf0b9cd06d32b910a8e9e13a5187b
Ričardas Bučiūnas <ricardas.buciunas@nortal.com>
2022-11-15 15:48:21 +0200

    Improving BeforeSuiteHook execution timing

--- test-automation-core/src/main/kotlin/com/nortal/test/core/cucumber/glue/HooksGlue.kt
+++ test-automation-core/src/main/kotlin/com/nortal/test/core/cucumber/glue/HooksGlue.kt
@@ -25,30 +25,19 @@ package com.nortal.test.core.cucumber.glue
 import com.nortal.test.core.services.ScenarioExecutionContext
 import com.nortal.test.core.services.hooks.AfterScenarioHook
 import com.nortal.test.core.services.hooks.BeforeScenarioHook
-import com.nortal.test.core.services.hooks.BeforeSuiteHook
 import io.cucumber.java.After
 import io.cucumber.java.Before
 import io.cucumber.java.Scenario
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.util.concurrent.locks.ReentrantLock
 import java.util.function.Consumer
 
 class HooksGlue(
     private val afterScenarioHooks: List<AfterScenarioHook>,
     private val beforeScenarioHooks: List<BeforeScenarioHook>,
-    private val suitePreparations: List<BeforeSuiteHook>,
     private val scenarioExecutionContext: ScenarioExecutionContext
 ) {
     private val log: Logger = LoggerFactory.getLogger(javaClass)
-
-
-    companion object {
-        private val initializationLock = ReentrantLock()
-
-        @Volatile
-        private var initialized = false
-    }
 
     /**
      * Cucumber does not expose a way to retrieve Scenario variable in the middle of a step, thus we must resort to grabbin it at the start and
@@ -60,30 +49,6 @@ class HooksGlue(
         scenarioExecutionContext.prepare(scenario)
     }
 
-    /**
-     * This hooks sets up our environment. Since cucumber does not support BeforeSuite hooks we use a boolean to run it once only.
-     */
-    @Before(order = 0)
-    fun configurationHook() {
-        if (initialized) {
-            return
-        }
-
-        try {
-            initializationLock.lock()
-            if (initialized) {
-                return
-            }
-            initialized = true
-
-            suitePreparations.stream()
-                .sorted(Comparator.comparingInt { obj: BeforeSuiteHook -> obj.beforeSuiteOrder() })
-                .peek { log.info("Running before suite hook for: {}", it.javaClass.name) }
-                .forEach { obj: BeforeSuiteHook -> obj.beforeSuite() }
-        } finally {
-            initializationLock.unlock()
-        }
-    }
 
     @Before(order = 1)
     fun beforeScenario() {
---
+++ test-automation-core/src/main/kotlin/com/nortal/test/core/services/SpringInitializationListener.kt
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2022 Nortal AS
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.nortal.test.core.services
+
+import com.nortal.test.core.services.hooks.BeforeSuiteHook
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ApplicationListener
+import org.springframework.stereotype.Component
+
+
+@Component
+class SpringInitializationListener : ApplicationListener<ApplicationReadyEvent> {
+    private val log: Logger = LoggerFactory.getLogger(javaClass)
+
+    override fun onApplicationEvent(event: ApplicationReadyEvent) {
+        event.applicationContext.getBeansOfType(BeforeSuiteHook::class.java).values.stream()
+            .sorted(Comparator.comparingInt { obj: BeforeSuiteHook -> obj.beforeSuiteOrder() })
+            .peek { log.info("Running before suite hook for: {}", it.javaClass.name) }
+            .forEach { obj: BeforeSuiteHook -> obj.beforeSuite() }
+    }
+}
