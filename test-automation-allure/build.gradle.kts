5397cfae2e87dc642477aa1eb920f1020d84289a
Ričardas Bučiūnas <ricardas.buciunas@nortal.com>
2022-11-28 21:58:49 +0200

    Dependency upgrade

--- test-automation-allure/build.gradle.kts
+++ test-automation-allure/build.gradle.kts
@@ -7,9 +7,16 @@ dependencies {
 
     implementation(project(":test-automation-core"))
 
-    implementation(libs.allure.cucumber)
-    implementation(libs.allure.plugin.api)
-    implementation(libs.allure.commandline)
+    implementation(libs.allure.cucumber) {
+        exclude(group = "org.slf4j", module = "slf4j-api")
+    }
+    implementation(libs.allure.plugin.api) {
+        exclude(group = "org.slf4j", module = "slf4j-api")
+    }
+    implementation(libs.allure.commandline) {
+        exclude(group = "org.slf4j", module = "slf4j-api")
+        exclude(group = "ch.qos.logback", module = "logback-classic")
+    }
 
     implementation(libs.commons.io)
 }
