e2b45a7d7fd0c45697d4ed774cae5279cb586b76
Ričardas Bučiūnas <ricardas.buciunas@nortal.com>
2022-12-01 15:18:12 +0200

    Made feign host overrider optional.

--- test-automation-feign/src/main/kotlin/com/nortal/test/feign/interceptor/TargetHostUpdateInterceptor.kt
+++ test-automation-feign/src/main/kotlin/com/nortal/test/feign/interceptor/TargetHostUpdateInterceptor.kt
@@ -27,21 +27,28 @@ import okhttp3.Interceptor
 import okhttp3.Response
 import org.springframework.stereotype.Component
 import java.io.IOException
+import java.util.*
 
 @Component
-open class TargetHostUpdateInterceptor(private val testableApplicationInfoProvider: TestableApplicationInfoProvider) : FeignClientInterceptor {
+open class TargetHostUpdateInterceptor(
+    private val testableApplicationInfoProvider: Optional<TestableApplicationInfoProvider>
+) : FeignClientInterceptor {
 
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
-        val newUrl = request.url.newBuilder()
-            .host(testableApplicationInfoProvider.getHost())
-            .port(testableApplicationInfoProvider.getPort())
-            .build()
 
-        request = request.newBuilder()
-            .url(newUrl)
-            .build()
+        testableApplicationInfoProvider.ifPresent {
+            val newUrl = request.url.newBuilder()
+                .host(it.getHost())
+                .port(it.getPort())
+                .build()
+
+            request = request.newBuilder()
+                .url(newUrl)
+                .build()
+        }
+
 
         return chain.proceed(request)
     }
