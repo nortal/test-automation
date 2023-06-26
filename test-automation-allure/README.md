8a58d6acd4b4ca28fa5693caf7f3e89b43b9f979
Vilmantas Runas <vilmantas.runas@nortal.com>
2023-06-26 13:40:17 +0300

    LTBENCH-157: Add an option to zip final report

--- test-automation-allure/README.md
+++ test-automation-allure/README.md
@@ -7,3 +7,5 @@ To use this module, add its dependency to your project.
 Report will be created after the tests are finished running.
 
 Generated report is accessible from: `build/allure-report/index.html`
+
+Allure report can be compressed into a zip file by adjusting the configuration properties accordingly.
---
+++ test-automation-allure/src/main/kotlin/com/nortal/test/report/allure/services/AllureReportZipService.kt
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2022 Nortal AS
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.nortal.test.report.allure.services
+
+import org.springframework.stereotype.Component
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+
+/**
+ * Compress Allure report to zip file.
+ */
+@Component
+class AllureReportZipService {
+
+    @Throws(IOException::class)
+    fun zipReport(reportDir: String, zipOutputDir: String) {
+        val reportToZip = File(reportDir)
+
+        val zipName = String.format("%s.zip", reportToZip.name)
+        val zipFile = File(zipOutputDir, zipName)
+        zipFile.parentFile.mkdirs()
+        val fos = FileOutputStream(zipFile)
+        val zipOut = ZipOutputStream(fos)
+
+        zipFile(reportToZip, reportToZip.name, zipOut)
+        zipOut.close()
+        fos.close()
+    }
+
+    @Throws(IOException::class)
+    private fun zipFile(fileToZip: File, fileName: String, zipOut: ZipOutputStream) {
+        if (fileToZip.isHidden) {
+            return
+        }
+        if (fileToZip.isDirectory) {
+            if (fileName.endsWith("/")) {
+                zipOut.putNextEntry(ZipEntry(fileName))
+                zipOut.closeEntry()
+            } else {
+                zipOut.putNextEntry(ZipEntry("$fileName/"))
+                zipOut.closeEntry()
+            }
+            val children = fileToZip.listFiles()
+            for (childFile in children!!) {
+                zipFile(childFile, fileName + "/" + childFile.name, zipOut)
+            }
+            return
+        }
+        val fis = FileInputStream(fileToZip)
+        val zipEntry = ZipEntry(fileName)
+        zipOut.putNextEntry(zipEntry)
+        val bytes = ByteArray(1024)
+        var length: Int
+        while (fis.read(bytes).also { length = it } >= 0) {
+            zipOut.write(bytes, 0, length)
+        }
+        fis.close()
+    }
+}
