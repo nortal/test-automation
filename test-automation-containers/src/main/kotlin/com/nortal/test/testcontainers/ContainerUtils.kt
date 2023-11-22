c10fa79b6e70b217fe694b4109db8f15d6db08ed
Ričardas Bučiūnas <ricardas.buciunas@nortal.com>
2023-11-22 11:21:36 +0200

    chore: fix reusable container compatability with testcontainers v1.19.3

--- test-automation-containers/src/main/kotlin/com/nortal/test/testcontainers/ContainerUtils.kt
+++ test-automation-containers/src/main/kotlin/com/nortal/test/testcontainers/ContainerUtils.kt
@@ -22,7 +22,6 @@
  */
 package com.nortal.test.testcontainers
 
-import org.apache.commons.lang3.reflect.FieldUtils
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.testcontainers.containers.GenericContainer
@@ -32,7 +31,7 @@ object ContainerUtils {
 
     fun overrideNetworkAliases(genericContainer: GenericContainer<*>?, aliases: List<String?>?) {
         try {
-            FieldUtils.writeField(genericContainer, "networkAliases", aliases, true)
+            genericContainer?.networkAliases = aliases
         } catch (e: IllegalAccessException) {
             log.error("Failed to override network alias", e)
         }
